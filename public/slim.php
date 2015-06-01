30b5ac90f8cef086523696e0abff109cb41d0696
Kyle Spraggs <theman@spiffyjr.me>
2015-06-01 16:45:58 -0500

    For realz, fixing slim

--- public/slim.php
+++ public/slim.php
@@ -1,6 +1,6 @@
 <?php
 require __DIR__ . '/../vendor/autoload.php';
-$_SERVER['SCRIPT_NAE'] = 'slim.php';
+$_SERVER['SCRIPT_NAME'] = 'slim.php';
 $app = new \Slim\Slim();
 $app->get('/hello/:name', function ($name) {
     echo "Slim: Hello, " . $name;
